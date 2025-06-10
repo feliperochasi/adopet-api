f4e86a969480ec7ff248ca7eebead7510ad0df9f
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-06-10 15:26:14 -0300

    Criacao do TutorService para as regras de negocio voltadas aos tutores

---
+++ src/main/java/br/com/alura/adopet/api/service/TutorService.java
@@ -0,0 +1,28 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Tutor;
+import br.com.alura.adopet.api.repository.TutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TutorService {
+    @Autowired
+    private TutorRepository repository;
+
+    public void cadastrar(Tutor tutor) {
+        boolean telefoneJaCadastrado = repository.existsByTelefone(tutor.getTelefone());
+        boolean emailJaCadastrado = repository.existsByEmail(tutor.getEmail());
+
+        if (telefoneJaCadastrado || emailJaCadastrado) {
+            throw new ValidacaoException("Dados já cadastrados para outro tutor!");
+        } else {
+            repository.save(tutor);
+        }
+    }
+
+    public void atualizar(Tutor tutor) {
+        repository.save(tutor);
+    }
+}
