9e0e85b7045f8890776b5ee85af240f457ab0fd1
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-06-11 11:30:57 -0300

    Implementacao da regra de negocio para criacao de um tutor atraves do padrao de projeto Strategy

--- src/main/java/br/com/alura/adopet/api/service/TutorService.java
+++ src/main/java/br/com/alura/adopet/api/service/TutorService.java
@@ -2,27 +2,29 @@ package br.com.alura.adopet.api.service;
 
 import br.com.alura.adopet.api.dto.AtualizaCadastroTutorDto;
 import br.com.alura.adopet.api.dto.CadastroDeTutorDto;
-import br.com.alura.adopet.api.exception.ValidacaoException;
 import br.com.alura.adopet.api.model.Tutor;
 import br.com.alura.adopet.api.repository.TutorRepository;
+import br.com.alura.adopet.api.validacoes.ValidacaoCadastroTutor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TutorService {
     @Autowired
     private TutorRepository repository;
 
-    public void cadastrar(CadastroDeTutorDto dto) {
-        boolean telefoneJaCadastrado = repository.existsByTelefone(dto.telefone());
-        boolean emailJaCadastrado = repository.existsByEmail(dto.email());
+    @Autowired
+    private List<ValidacaoCadastroTutor> validacoes;
 
-        if (telefoneJaCadastrado || emailJaCadastrado) {
-            throw new ValidacaoException("Dados já cadastrados para outro tutor!");
-        } else {
-            Tutor tutor = new Tutor(dto);
-            repository.save(tutor);
-        }
+    public void cadastrar(CadastroDeTutorDto dto) {
+
+        validacoes.forEach(v -> v.validar(dto));
+
+        Tutor tutor = new Tutor(dto);
+        repository.save(tutor);
+
     }
 
     public void atualizar(AtualizaCadastroTutorDto dto) {
