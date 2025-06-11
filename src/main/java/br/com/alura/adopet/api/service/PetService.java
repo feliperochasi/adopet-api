b1bf9549dd866d32d182a232e87e753a1ddfe62d
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-06-11 13:56:09 -0300

    Refatoracao do metodo para retornar somente pets disponiveis para adocao

--- src/main/java/br/com/alura/adopet/api/service/PetService.java
+++ src/main/java/br/com/alura/adopet/api/service/PetService.java
@@ -6,7 +6,6 @@ import br.com.alura.adopet.api.repository.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,13 +15,7 @@ public class PetService {
     private PetRepository repository;
 
     public List<DetalhesPetDto> listarTodosDisponiveis () {
-        List<Pet> pets = repository.findAll();
-        List<DetalhesPetDto> disponiveis = new ArrayList<>();
-        for (Pet pet : pets) {
-            if (pet.getAdotado() == false) {
-                disponiveis.add(new DetalhesPetDto(pet));
-            }
-        }
-        return disponiveis;
+        List<Pet> pets = repository.findByAdotado(false);
+        return pets.stream().map(DetalhesPetDto::new).toList();
     }
 }
